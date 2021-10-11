69089169415dccf308b85fcb4dc0c1f4f8a1e8df
Wen Xiao <wennstyle@gmail.com>
2021-10-10 22:58:35 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (task) => {
+    task.preventDefault()
+    handleToDo(task.target.new_task.value)
+    form.reset()
+  })
 });
+
+function handleToDo(todo){
+
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' Mark Completed'
+  li.textContent = todo
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
